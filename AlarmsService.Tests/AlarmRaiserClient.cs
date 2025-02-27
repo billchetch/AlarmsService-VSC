d733b1c81fd4767983b82e5a99e28671c2dddf21
bill@chetch.net <bill@chetch.net>
2025-02-28 00:19:53 +0800

    asdf

--- AlarmsService.Tests/AlarmRaiserClient.cs
+++ AlarmsService.Tests/AlarmRaiserClient.cs
@@ -1,19 +1,52 @@
 ﻿using System.Diagnostics;
 using System.Threading.Tasks;
 using Chetch.ChetchXMPP;
+using Chetch.Alarms;
 
 namespace AlarmsService.Tests;
 
 [TestClass]
-public sealed class AlarmRaiserClient
+public sealed class AlarmRaiserClient : AlarmManager.IAlarmRaiser
 {
     const String USERNAME = "mactest@openfire.bb.lan";
     const String PASSWORD = "mactest";
 
+    const String ALARMS_SERVICE = "bbalarms.service@openfire.bb.lan";
+
+    ChetchXMPPConnection cnn;
+
+    public AlarmManager AlarmManager { get; set; } = new AlarmManager();
+
+    public AlarmRaiserClient()
+    {
+        cnn = new ChetchXMPPConnection(USERNAME, PASSWORD);
+        //cnn.MessageReceived += ()
+
+        AlarmManager.AddRaiser(this);
+        AlarmManager.AlarmChanged += (sender, alarm) => {
+            Debug.Print("Alarm changed event");
+
+            try
+            {
+                var msg = AlarmManager.CreateAlertMessage(alarm, ALARMS_SERVICE);
+                cnn.SendMessageAsync(msg);
+                Debug.Print("Sent alarm alert for {0} to alarms service {1}", alarm.ID, ALARMS_SERVICE);
+            }
+            catch (Exception e)
+            {
+                Debug.Print("Error: {0}", e.Message);
+            }
+            
+        };
+    }
+
+    public void RegisterAlarms()
+    {
+        AlarmManager.RegisterAlarm(this, "gps", "GPS Alarm Test");
+    }
 
     Task connectClient()
     {
-        var cnn = new ChetchXMPPConnection(USERNAME, PASSWORD);
         return cnn.ConnectAsync();
     }
 
@@ -23,6 +56,7 @@ public sealed class AlarmRaiserClient
         Debug.Print("Connecting client...");
         await connectClient();
         Debug.Print("Client connected");
-
+        AlarmManager.Raise("gps", AlarmManager.AlarmState.MODERATE, "Fuck its working");
+        await Task.Delay(2000);
     }
 }
