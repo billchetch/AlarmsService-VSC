7d8b5f6eb77d0f40fcd2b8cea8f9e949c996f7da
bill@chetch.net <bill@chetch.net>
2025-02-28 00:00:10 +0800

    asdfsdf

---
+++ AlarmsService.Tests/AlarmRaiserClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Chetch.ChetchXMPP;
+
+namespace AlarmsService.Tests;
+
+[TestClass]
+public sealed class AlarmRaiserClient
+{
+    const String USERNAME = "mactest@openfire.bb.lan";
+    const String PASSWORD = "mactest";
+
+
+    Task connectClient()
+    {
+        var cnn = new ChetchXMPPConnection(USERNAME, PASSWORD);
+        return cnn.ConnectAsync();
+    }
+
+    [TestMethod]
+    public async Task TestMethod1()
+    {
+        Debug.Print("Connecting client...");
+        await connectClient();
+        Debug.Print("Client connected");
+
+    }
+}
