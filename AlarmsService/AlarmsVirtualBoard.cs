e502874bae85f94669daab57ea893812e2b3df5b
billchetch <45006134+billchetch@users.noreply.github.com>
2025-07-23 23:07:01 +0800

    asdf

--- AlarmsService/AlarmsVirtualBoard.cs
+++ AlarmsService/AlarmsVirtualBoard.cs
@@ -12,35 +12,35 @@ public class AlarmsVirtualBoard : ArduinoVirtualBoard
     {
         var regime = new ArduinoVirtualBoard.Regime("local-alarms");
         regime.RepeatCount = 1;
-        regime.AddMessage(board.GensetAlarm, MessageType.DATA, "PinState", 1);
+        regime.AddMessage(board.GensetAlarm, MessageType.DATA, "PinState", true);
         regime.AddDelay(1000);
-        regime.AddMessage(board.InverterAlarm, MessageType.DATA, "PinState", 1);
+        regime.AddMessage(board.InverterAlarm, MessageType.DATA, "PinState", true);
         regime.AddDelay(1000);
-        regime.AddMessage(board.HighwaterAlarm, MessageType.DATA, "PinState", 1);
+        regime.AddMessage(board.HighwaterAlarm, MessageType.DATA, "PinState", true);
         regime.AddDelay(1000);
-        regime.AddMessage(board.GensetAlarm, MessageType.DATA, "PinState", 0);
+        regime.AddMessage(board.GensetAlarm, MessageType.DATA, "PinState", false);
         regime.AddDelay(1000);
-        regime.AddMessage(board.InverterAlarm, MessageType.DATA, "PinState", 0);
+        regime.AddMessage(board.InverterAlarm, MessageType.DATA, "PinState", false);
         regime.AddDelay(1000);
-        regime.AddMessage(board.HighwaterAlarm, MessageType.DATA, "PinState", 0);
+        regime.AddMessage(board.HighwaterAlarm, MessageType.DATA, "PinState", false);
         AddRegime(regime);
 
         foreach (var la in board.LocalAlarms)
         {
             regime = new ArduinoVirtualBoard.Regime(la.SID + "-alarm");
             regime.RepeatCount = 3;
-            regime.AddMessage(la, MessageType.DATA, "PinState", 1);
+            regime.AddMessage(la, MessageType.DATA, "PinState", true);
             regime.AddDelay(1000);
-            regime.AddMessage(la, MessageType.DATA, "PinState", 0);
+            regime.AddMessage(la, MessageType.DATA, "PinState", false);
             regime.AddDelay(1000);
             AddRegime(regime);
         }
 
         regime = new ArduinoVirtualBoard.Regime(board.GensetAlarm.SID + "-alarm-20s");
         regime.RepeatCount = 2;
-        regime.AddMessage(board.GensetAlarm, MessageType.DATA, "PinState", 1);
+        regime.AddMessage(board.GensetAlarm, MessageType.DATA, "PinState", true);
         regime.AddDelay(20000);
-        regime.AddMessage(board.GensetAlarm, MessageType.DATA, "PinState", 0);
+        regime.AddMessage(board.GensetAlarm, MessageType.DATA, "PinState", false);
         regime.AddDelay(2000);
         AddRegime(regime);
     }
